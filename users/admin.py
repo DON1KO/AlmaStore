2100458353d53220263814c28f31b9538f22d0cf
Daniel <shailoobekovdaniel01@gmail.com>
2026-07-08 09:39:34 +0000

    Add users/admin.py

---
+++ users/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+
+@admin.register(User1)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('phone_number', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2'),
+        }),
+    )
+    ordering = ('phone_number',)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'city')
+
+@admin.register(SMSVerification)
+class SMSVerificationAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'code', 'created_at', 'is_used')
+
+admin.site.register(BonusCard)
+admin.site.register(Notification)
